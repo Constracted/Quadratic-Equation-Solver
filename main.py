6a6438039a7e61a2288162f91ba903e418691f33
Constracted <>
2021-05-18 14:51:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This program will calculate the roots of a quadratic equation.
+
+def root_one(a, b, c):
+    """This function returns the first root of a quadratic equation."""
+    return (-b + (b ** 2 - (4 * a * c)) ** 0.5) / (2 * a)
+
+
+def root_two(a, b, c):
+    """This function returns the second root of a quadratic equation."""
+    return (-b - (b ** 2 - (4 * a * c)) ** 0.5) / (2 * a)
+
+
+a = int(input("Insert the 'a' value: "))
+b = int(input("Insert the 'b' value: "))
+c = int(input("Insert the 'c' value: "))
+print()
+
+if (b ** 2 - 4 * a * c) == 0:
+    print("Both roots of the equation are equal to", root_one(a, b, c))
+else:
+    print("Root one = ", root_one(a, b, c))
+    print()
+    print("Root two = ", root_two(a, b, c))
